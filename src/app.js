50066accafb17272b9faaf862fd21aa8531274c9
egonixaimgod <bodnarbence2002@gmail.com>
2023-05-19 10:04:39 +0200

    Kész, még névjegy nélkül.

---
+++ src/app.js
@@ -0,0 +1,46 @@
+
+
+const doc = {
+    tbody: document.querySelector('#tbody')
+};
+
+const state = {
+    host: 'http://localhost:8000/',
+    ships: []
+};
+
+window.addEventListener('load', () => {    
+    init();
+});
+
+function init() {
+    getShips();
+}
+
+function getShips() {    
+    let endpoint = "ships";
+    let url = state.host + endpoint;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {        
+        state.ships = result;
+        render();
+    });
+}
+
+function render() {
+    var rows = "";
+    state.ships.forEach( ship => {
+        var row = `
+        <tr>
+            <td>${ship.name}</td>
+            <td>${ship.length}</td>
+            <td>${ship.price}</td>
+            <td>${ship.person}</td>
+            <td>${ship.trailer}</td>
+        </tr>
+        `;
+        rows += row;        
+    })
+    doc.tbody.innerHTML = rows;
+}
